7980853c94ae63daadad70dd6a096869af8ea820
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-10-19 00:22:39 +0800

    add useClickOutside hook
    - apply to LanguageDropdown

--- src/layouts/Layout/Header/LanguageDropdown/index.jsx
+++ src/layouts/Layout/Header/LanguageDropdown/index.jsx
@@ -1,7 +1,9 @@
 // 樣式模組 (css module)
 import S from './style.module.css'
 // 函式庫 (library)
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+// 自訂函式 (custom function)
+import useClickOutside from '../../../../hooks/useClickOutside'
 // 組件 (component)
 import Icon from '../../../../components/Icon'
 
@@ -10,10 +12,14 @@ function LanguageDropdown() {
   const [showDropdown, setShowDropdown] = useState(false)
   const toggleDropdown = () => setShowDropdown((prevState) => !prevState)
 
+  const dropdownRef = useRef(null)
+  useClickOutside(dropdownRef, () => setShowDropdown(false))
+
   const currentLang = 'zh'
 
   return (
-    <div className={S.languageDropdown} onClick={toggleDropdown}>
+    <div className={S.languageDropdown} onClick={toggleDropdown} ref={dropdownRef}>
+      {/* 語言按鈕 */}
       <button className={`${S.dropdownButton} ${showDropdown ? S.flip : ''}`}>
         <div className={S.front}>
           <Icon style={S.earthAmericas} icon="faEarthAmericas" />
@@ -22,6 +28,7 @@ function LanguageDropdown() {
           <Icon style={S.earthAsia} icon="faEarthAsia" />
         </div>
       </button>
+      {/* 下拉選單 */}
       <ul
         className={`${S.dropdown} ${showDropdown ? S.show : ''}`}
         onClick={(e) => e.stopPropagation()}
