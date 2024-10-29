b01cce1852e478ec2adcd7968567ca4797695617
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-10-29 13:13:56 +0800

    update workflow

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,8 +3,11 @@ import S from './style.module.css'
 
 import Anchor from '../../components/Anchor'
 
+const { VITE_TEST } = import.meta.env
+
 // 首頁
 function Home() {
+  console.log(VITE_TEST)
   return (
     <main className={S.main}>
       <Anchor int="/sign-in">Sign In</Anchor>
